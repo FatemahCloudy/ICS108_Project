eeac6d4096c6b4432480f47f758507489d55e53b
Fatemah Almarhoon <165394686+FatemahCloudy@users.noreply.github.com>
2024-04-24 08:22:34 +0300

    Create Ticket.java

---
+++ Ticket.java
@@ -0,0 +1,32 @@
+public class Ticket {
+    private Event event;
+    private String purchaserName;
+    private int seatNumber;
+
+    public Ticket(Event event, String purchaserName, int seatNumber) {
+        this.event = event;
+        this.purchaserName = purchaserName;
+        this.seatNumber = seatNumber;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public String getPurchaserName() {
+        return purchaserName;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "eventTitle='" + event.getTitle() + '\'' +
+                ", purchaserName='" + purchaserName + '\'' +
+                ", seatNumber=" + seatNumber +
+                '}';
+    }
+}
