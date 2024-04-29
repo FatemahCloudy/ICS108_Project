5d0b75ac93c1e3577269b630de16ab0bfe8ce8e7
Fatemah Almarhoon <165394686+FatemahCloudy@users.noreply.github.com>
2024-04-29 17:38:58 +0300

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,5 +1,5 @@
 package com.example.ics108_project;
-//ToDo: show a message for the user if he tried to book tickets more than available
+//ToDo: show a message for the user if he tried to book tickets more than available (Done!)
 public class Event {
     private String title;
     private String category;
@@ -20,20 +20,20 @@ public class Event {
         this.capacity = capacity;
         this.bookedTickets = 0;
     }
+    // Did the user just booked some tickets? Increment the number of booked tickets
+    public void bookTickets(int numberOfTickets) {bookedTickets+= numberOfTickets;}
+    
+    // A useful method to see if there is enough tickets to book or no
+    public int availableTickets() {return capacity - bookedTickets;}
 
-public void bookTickets(int numberOfTickets) {bookedTickets+= numberOfTickets;}
-
-public Ticket book(String purchaserName) { // for the user
-    if (availableTickets() > 1) {
-        int seatNumber = capacity - availableTickets() + 1;
-        bookTickets(1);
-        return new Ticket(this, purchaserName, seatNumber);
-
-    } else {return null;} // No tickets available
-}
-
-    public int availableTickets() {
-        return capacity - bookedTickets;
+    // ToDo: This method was never used, it is the one that generate tickets
+    public Ticket book(String purchaserName) {
+        if (availableTickets() > 1) {
+            int seatNumber = capacity - availableTickets() + 1;
+            bookTickets(1);
+            return new Ticket(this, purchaserName, seatNumber);
+    
+        } else {return null;} // No tickets available
     }
 
     @Override
@@ -49,38 +49,36 @@ public Ticket book(String purchaserName) { // for the user
     }
 
     // Admin methods to edit the event details
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public void setTitle(String title) {this.title = title;}
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
+    public void setCategory(String category) {this.category = category;}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public void setDescription(String description) {this.description = description;}
 
     public void setDate(String date) {this.date = date;}
 
     public void setTime(String time) {this.time = time;}
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+    public void setLocation(String location) {this.location = location;}
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
+    public void setCapacity(int capacity) {this.capacity = capacity;}
 
-    // getters
+    // getters for everything. To be used across classes
     public String getTitle() {return title;}
+    
     public String getDate() {return date;}
+    
     public String getTime() {return time;}
+    
     public int getCapacity() {return capacity;}
+    
     public String getCategory() {return category;}
+    
     public String getDescription() {return description;}
+    
     public String getLocation() {return location;}
+    
     public int getBookedTickets() {return bookedTickets;}
+    
     public int getAvailableTickets() {return capacity - bookedTickets;}
 }
