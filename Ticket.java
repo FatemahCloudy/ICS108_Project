2470e73a25617a2aa40b78d975da9dca1b3b0db4
Fatemah Almarhoon <165394686+FatemahCloudy@users.noreply.github.com>
2024-04-29 17:40:46 +0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,4 +1,5 @@
 package com.example.ics108_project;
+// This is a record class, just to store the information of each ticket the user book
 
 public record Ticket(Event event, String purchaserName, int seatNumber) {
 
