afec717d9390f611f3fecfed242ea829f8c832d7
Fatemah Almarhoon <165394686+FatemahCloudy@users.noreply.github.com>
2024-04-30 20:10:06 +0300

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,6 +1,32 @@
-public class Event {
-    // ... existing fields and methods ...
+package com.example.ics108_project;
 
+import java.time.LocalDateTime;
+
+public class Event {
+    private String title;
+    private String category;
+    private String description;
+    LocalDateTime dateTime;
+    private String location;
+    private int capacity;
+    private int bookedTickets;
+    //ToDo: edit the date type
+    public Event(String title, String category, String description, LocalDateTime dateTime, String location, int capacity) {
+        this.title = title;
+        this.category = category;
+        this.description = description;
+        this.dateTime = dateTime;
+        this.location = location;
+        this.capacity = capacity;
+        this.bookedTickets = 0;
+    }
+    // Did the user just booked some tickets? Increment the number of booked tickets
+    public void bookTickets(int numberOfTickets) {bookedTickets+= numberOfTickets;}
+
+    // A useful method to see if there is enough tickets to book or no
+    public int availableTickets() {return capacity - bookedTickets;}
+
+    // ToDo: This method was never used, it is the one that generate tickets
     public Ticket book(String purchaserName, int numTickets) {
         if (availableTickets() >= numTickets) {
             bookTickets(numTickets); // This method would update the number of bookedTickets.
@@ -11,6 +37,46 @@ public class Event {
             return null;
         }
     }
-    
-    // ... rest of the Event class ...
+
+    @Override
+    public String toString() {
+        return " title:'" + title + '\'' +
+                ", category:'" + category + '\'' +
+                ", description: '" + description + '\'' +
+                ", date: " + dateTime.toLocalDate() +
+                ", time: " + dateTime.toLocalTime() +
+                ", location: '" + location + '\'' +
+                ", capacity: " + capacity +
+                ", availableTickets: " + availableTickets();
+    }
+
+    // Admin methods to edit the event details
+    public void setTitle(String title) {this.title = title;}
+
+    public void setCategory(String category) {this.category = category;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public void setDateTime(LocalDateTime dateTime) {this.dateTime = dateTime;}
+
+    public void setLocation(String location) {this.location = location;}
+
+    public void setCapacity(int capacity) {this.capacity = capacity;}
+
+    // getters for everything. To be used across classes
+    public String getTitle() {return title;}
+
+    public LocalDateTime getDateTime() {return dateTime;}
+
+    public int getCapacity() {return capacity;}
+
+    public String getCategory() {return category;}
+
+    public String getDescription() {return description;}
+
+    public String getLocation() {return location;}
+
+    public int getBookedTickets() {return bookedTickets;}
+
+    public int getAvailableTickets() {return capacity - bookedTickets;}
 }
