b47ec2fd2dea8596fef32e26ec05deed986560ff
Fatemah Almarhoon <165394686+FatemahCloudy@users.noreply.github.com>
2024-04-30 16:15:57 +0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,14 +1,17 @@
 package com.example.ics108_project;
+
 // This is a record class, just to store the information of each ticket the user book
 
-public record Ticket(Event event, String purchaserName, int seatNumber) {
+public record Ticket(Event event, String purchaserName, int numTickets) {
 
     @Override
     public String toString() {
         return "Ticket{" +
-                "eventTitle: '" + event.getTitle() + '\'' +
-                ", purchaserName: '" + purchaserName + '\'' +
-                ", seatNumber: " + seatNumber +
+                "event: '" + event.getTitle() + '\'' +
+                ", purchaser: '" + purchaserName + '\'' +
+                ", number of tickets: " + numTickets +
                 '}';
     }
+    public Event getEvent() {return event;}
+    public int getNumTickets() {return numTickets;}
 }
