6de709e4c4e00d0b0de689ff0cdfa4966956a6f3
Fatemah Almarhoon <165394686+FatemahCloudy@users.noreply.github.com>
2024-04-30 20:10:35 +0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,15 +1,13 @@
 package com.example.ics108_project;
 
+// This is a record class, just to store the information of each ticket the user book
+
 import java.time.LocalDateTime;
 
-public record Ticket(Event event, String purchaserName, int numTickets, LocalDateTime bookingTime) {
-
-    // Constructor for Ticket, automatically provided by the record.
-    // The bookingTime field has been added to track when the ticket was booked.
+public record Ticket(Event event, String purchaserName, int numTickets, LocalDateTime dateTime) {
 
     public Ticket(Event event, String purchaserName, int numTickets) {
-        this(event, purchaserName, numTickets, LocalDateTime.now());
-        // Calls the canonical constructor to set the booking time to the current time.
+        this(event, purchaserName, numTickets, LocalDateTime.now()); //set the booking time to the current time
     }
 
     @Override
@@ -18,27 +16,8 @@ public record Ticket(Event event, String purchaserName, int numTickets, LocalDat
                 "event: '" + event.getTitle() + '\'' +
                 ", purchaser: '" + purchaserName + '\'' +
                 ", number of tickets: " + numTickets +
-                ", booking time: " + bookingTime +
                 '}';
     }
-
-    // The getters for event, purchaserName, and numTickets are automatically generated.
-    // However, if you need to explicitly define or override them, you can do so.
-    // For example:
-
-    public Event getEvent() {
-        return event;
-    }
-
-    public String getPurchaserName() {
-        return purchaserName;
-    }
-
-    public int getNumTickets() {
-        return numTickets;
-    }
-
-    public LocalDateTime getBookingTime() {
-        return bookingTime;
-    }
+    public Event getEvent() {return event;}
+    public int getNumTickets() {return numTickets;}
 }
