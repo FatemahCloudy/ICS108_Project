8845c85b59556bb4be83e483ddb6fe48ecceb2d1
WAJD ALGHAMDI <128381490+Wajza@users.noreply.github.com>
2024-04-30 19:44:47 +0300

    Update UserScene.java

--- UserScene.java
+++ UserScene.java
@@ -2,23 +2,22 @@ package com.example.ics108_project;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 // ... other imports ...
 
-public class UserScene {
+public class AdminScene {
     private Stage stage;
-    private Scene userScene; // The Scene for this view
+    private Scene adminScene; // The Scene for this view
     // ... other fields ...
 
     // Constructor remains the same
-    public UserScene(Stage stage) {
+    public AdminScene(Stage stage) {
         this.stage = stage;
         events = FXCollections.observableArrayList();
         initialize();
@@ -29,14 +28,14 @@ public class UserScene {
         // ... field initializations and layout setup ...
 
         // Instead of showing the stage, simply create the Scene and assign it
-        userScene = new Scene(layout, 400, 300);
+        adminScene = new Scene(layout, 600, 400);
     }
 
-    // ... other methods including setEvents, displayEventDetails, bookTickets ...
+    // ... other methods ...
 
-    // Method to get the Scene object for this UserScene
-    public Scene getUserScene() {
-        return userScene;
+    // Method to get the Scene object for this AdminScene
+    public Scene getAdminScene() {
+        return adminScene;
     }
 
     // ... rest of the class code ...
