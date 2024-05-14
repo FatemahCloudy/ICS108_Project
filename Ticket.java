3e980af9a6efbfb616333ef6ed61c46e5a4717d5
Fatemah Almarhoon <165394686+FatemahCloudy@users.noreply.github.com>
2024-05-14 18:35:15 +0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,31 +1,25 @@
 package com.eventbookingsystem;
 
 /**
- * Ticket class to represent a ticket for an event within the event booking system.
- * Each ticket is associated with a specific event and has quantity
+ * Ticket record class to represent a ticket for an event.
+ * Each ticket is associated with an event and have details about the date and time along with the quantity
  */
 
-// This is a record class, just to store the information of each ticket the user book
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public record Ticket(Event event, int numTickets, LocalDate startDate,
                      LocalTime startTime, LocalDate endDate, LocalTime endTime) {
 
     /**
-     * Method to Provides a string representation of the ticket, detailing the number of tickets,
-     * the title of the event, and other relevant event details.
+     * Method to represent the ticket as a string, detailing the title of the event
+     * and the number of tickets.
      */
 
     @Override
     public String toString() {
-        return "Ticket{" +
+        return "Ticket: " +
                 "event: '" + event.getTitle() + '\'' +
-                ", number of tickets: " + numTickets +
-                '}';
+                ", number of tickets: " + numTickets;
     }
-    public Event getEvent() {return event;}
-    public int getNumTickets() {return numTickets;}
 }
